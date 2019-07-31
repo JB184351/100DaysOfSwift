a8c6aaa7602a01a8cb51587307c5518626eb90fb
JB184351 <justinbengtson@icloud.com>
2019-07-31 13:16:55 -0700

    Day 20

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Justin Bengtson on 7/30/19.
+//  Copyright © 2019 Justin Bengtson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var scores = 0
+    var correctAnswer = 0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+        
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        
+        countries.shuffle()
+        
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            scores += 1
+        }
+        
+        else {
+            title = "Wrong"
+            scores -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(scores)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
+    
+    
+
+
+}
+
