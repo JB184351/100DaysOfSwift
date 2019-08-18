c48c53092182a8d6b58e40d636b82902622e3535
JB184351 <justinbengtson@icloud.com>
2019-08-17 18:08:28 -0700

    Day 34 Working on Day 35 Challenge 2

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -11,22 +11,41 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        // Challenge 1
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredit))
+        
+        // Challenge 2
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchPetitions))
+            
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        }
+        else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
               // We're good to parse
               parse(json: data)
-                
+              return
             }
         }
         
-        
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
@@ -34,26 +53,67 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
+        
         return cell
     }
     
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
     
+    // Challenge 1
+    @objc func showCredit() {
+        let ac = UIAlertController(title: "Source of Information", message: "The data comes from the We The People API of the Whitehouse.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func searchPetitions() {
+        let ac = UIAlertController(title: "Filter Petitions", message: nil, preferredStyle: .alert)
+        
+        ac.addTextField()
+        
+        let searchAction = UIAlertAction(title: "Search", style: .default) {
+            [weak self, weak ac] action in
+            guard let searchItem = ac?.textFields?[0].text else { return }
+            self?.filterPetitions(search: searchItem)
+        }
+
+        ac.addAction(searchAction)
+        present(ac, animated: true)
+        
+        
+    }
+    
+    func filterPetitions(search: String) {
+        for petition in petitions {
+            if petition.title.contains(search) || petition.body.contains(search) {
+                filteredPetitions.append(petition)
+            }
+        }
+        
+    }
+    
+    
+ 
 
 }
 
